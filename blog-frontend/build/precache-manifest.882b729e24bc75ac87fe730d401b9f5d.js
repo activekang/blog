0f48f472c05395115b14ada32c9944d11893829f
activekang <33951519+activekang@users.noreply.github.com>
2022-07-03 14:20:32 +0900

    Add files via upload

---
+++ blog-frontend/build/precache-manifest.882b729e24bc75ac87fe730d401b9f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b0192a6a9881f00d3134e5af61841c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05f5454e9e85f0883f15",
+    "url": "/static/css/2.fa5fcd43.chunk.css"
+  },
+  {
+    "revision": "2810ff6214e42c69f1aa",
+    "url": "/static/css/main.0d804af2.chunk.css"
+  },
+  {
+    "revision": "05f5454e9e85f0883f15",
+    "url": "/static/js/2.4dd2f6dd.chunk.js"
+  },
+  {
+    "revision": "2810ff6214e42c69f1aa",
+    "url": "/static/js/main.067a9f83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
